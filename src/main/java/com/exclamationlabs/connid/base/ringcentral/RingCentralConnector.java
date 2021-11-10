d84d18df7c7a995ac00e7eca051860122df1fd38
Mike Neugebauer <mneugebauer@exclamationlabs.com>
2021-11-10 15:28:11 -0600

    EAGLE-330 - 0.6 version - configurable preferred call queue ids; fixed containsAll filtering

--- src/main/java/com/exclamationlabs/connid/base/ringcentral/RingCentralConnector.java
+++ src/main/java/com/exclamationlabs/connid/base/ringcentral/RingCentralConnector.java
@@ -13,7 +13,9 @@
 
 package com.exclamationlabs.connid.base.ringcentral;
 
+import com.exclamationlabs.connid.base.connector.BaseConnector;
 import com.exclamationlabs.connid.base.connector.BaseFullAccessConnector;
+import com.exclamationlabs.connid.base.connector.adapter.BaseAdapter;
 import com.exclamationlabs.connid.base.connector.authenticator.OAuth2TokenPasswordAuthenticator;
 import com.exclamationlabs.connid.base.ringcentral.adapter.RingCentralCallQueueAdapter;
 import com.exclamationlabs.connid.base.ringcentral.adapter.RingCentralUsersAdapter;
@@ -21,6 +23,12 @@ import com.exclamationlabs.connid.base.ringcentral.attribute.RingCentralCallQueu
 import com.exclamationlabs.connid.base.ringcentral.attribute.RingCentralUserAttribute;
 import com.exclamationlabs.connid.base.ringcentral.configuration.RingCentralConfiguration;
 import com.exclamationlabs.connid.base.ringcentral.driver.rest.RingCentralDriver;
+import org.apache.commons.lang3.StringUtils;
+import org.identityconnectors.common.StringUtil;
+import org.identityconnectors.framework.common.exceptions.ConnectorException;
+import org.identityconnectors.framework.common.exceptions.InvalidAttributeValueException;
+import org.identityconnectors.framework.common.objects.*;
+import org.identityconnectors.framework.common.objects.filter.*;
 import org.identityconnectors.framework.spi.ConnectorClass;
 
 import java.util.HashSet;
@@ -41,5 +49,71 @@ public class RingCentralConnector extends BaseFullAccessConnector {
         filterSet.add(RingCentralCallQueueAttribute.USER_MEMBERS.name());
         setFilterAttributes(filterSet);
     }
+
+    @Override
+    protected FilterTranslator<String> getConnectorFilterTranslator(ObjectClass objectClass) {
+        BaseAdapter<?> matchedAdapter = adapterMap.get(objectClass);
+        if (matchedAdapter == null) {
+            throw new ConnectorException("Unsupported object class for filter translator: " + objectClass);
+        } else {
+            return new CustomFilterTranslator(getFilterAttributes());
+        }
+    }
+
+    public static class CustomFilterTranslator extends AbstractFilterTranslator<String> {
+
+        private final Set<String> acceptableAttributeNames;
+
+        public CustomFilterTranslator(Set<String> attributes) {
+            acceptableAttributeNames = attributes;
+        }
+
+        @Override
+        // Not normally invoked by Midpoint
+        protected String createEqualsExpression(EqualsFilter filter, boolean not) {
+            if (not || filter == null) {
+                return null;
+            }
+
+            Attribute attr = filter.getAttribute();
+            if (!attr.is(Name.NAME) && !attr.is(Uid.NAME)) {
+                return null;
+            }
+            String value = AttributeUtil.getAsStringValue(attr);
+
+            return (checkSearchValue(value) == null) ? null : value;
+        }
+
+        @Override
+        // Normally invoked by Midpoint for filtering
+        protected String createContainsExpression(ContainsFilter filter, boolean not) {
+            if (StringUtils.equals(filter.getAttribute().getName(), Uid.NAME) || acceptableAttributeNames.contains(filter.getAttribute().getName())) {
+                return filter.getAttribute().getName() + BaseConnector.FILTER_SEPARATOR + filter.getValue();
+            } else {
+                throw new InvalidAttributeValueException("Filter on attribute " + filter.getAttribute().getName() + " not supported.");
+            }
+        }
+
+        @Override
+        protected String createContainsAllValuesExpression(ContainsAllValuesFilter filter, boolean not) {
+            if (StringUtils.equals(filter.getAttribute().getName(), Uid.NAME) || acceptableAttributeNames.contains(filter.getAttribute().getName())) {
+                return filter.getAttribute().getName() + BaseConnector.FILTER_SEPARATOR + filter.getAttribute().getValue().get(0);
+            } else {
+                throw new InvalidAttributeValueException("Filter on attribute " + filter.getAttribute().getName() + " not supported.");
+            }
+        }
+
+        private String checkSearchValue(String value) {
+            if (StringUtil.isEmpty(value)) {
+                return null;
+            }
+            if (value.contains("*") || value.contains("&") || value.contains("|")) {
+                throw new IllegalArgumentException(
+                        "Value of search attribute contains illegal character(s).");
+            }
+            return value;
+        }
+    }
+
 
 }
